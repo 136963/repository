bf269e7bc7d5623884f990da9eb8701289b6d508
136963 <136963@estudante.se.df.gov.br>
2026-04-08 22:02:15 -0300

    Mudança

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
-  <title>Meu Site</title>
+  <title>Meu Novo Site</title>
 </head>
 <body>
-    <?php echo "<h1>Olá, mundo!</h1>" ?>
+    <?php echo "<h1>Tchau, mundo!</h1>" ?>
 </body>
 </html>
